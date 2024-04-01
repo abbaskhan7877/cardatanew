d85c81bf53e1e5b26bcc2dc35924d1d6873b3ef7
abbaskhan7877 <kajiabbas733@gmail.com>
2024-04-01 16:14:10 +0530

    second

---
+++ index.js
@@ -0,0 +1,94 @@
+const carsObj = [
+    {
+        agencyName: "Noor Travelers",
+        carName: "Dzire, Crysta, Etios, Innova, Ertiga, Verna, Swift  ",
+        number: "9887951071",
+    },
+    {
+        agencyName: "Ragiv Travelers",
+        carName: "Dzire, Swift, Boloro, Scorpio  ",
+        number: "9785284537",
+    },
+    {
+        agencyName: "Sk Travelers",
+        carName: "Dzire, Ertiga  ",
+        number: "9664471705",
+    },
+    {
+        agencyName: "Lukman Travelers",
+        carName: "Dzire, Ertiga, Swift  ",
+        number: "737828158",
+    },
+    {
+        agencyName: "Jm Travelers",
+        carName: "Thar, BMW, Fortuner, All Car`s",
+        number: "9649044141",
+    },
+    {
+        agencyName: "Aabdesh ",
+        carName: "Swift, Dzire",
+        number: "8619386606",
+    },
+    {
+        agencyName: "Imran khan",
+        carName: "Swift, Innova",
+        number: "8432629203",
+    },
+    {
+        agencyName: "Raju",
+        carName: "Ertiga, Swift",
+        number: "9352666814",
+    },
+    {
+        agencyName: "Sfrati",
+        carName: "Tabira, Van, Wagnar",
+        number: "9982646022",
+    },
+    {
+        agencyName: "Nannan",
+        carName: "Ertiga, Scorpio",
+        number: "9982646022",
+    },
+
+]
+const busobj = [
+    {
+        agencyName: "Noor Travelers",
+        Name: "Kalpana, Kalpana, Kalpana AC Bus  ",
+        number: "9887999874",
+    },
+    {
+        agencyName: "Golu Travelers",
+        Name: "Janata, Janata ",
+        number: "9785715909",
+    },
+    {
+        agencyName: "Noor Travelers ",
+        Name: "Tourist, mini bus",
+        number: "9887999874",
+    },
+]
+const transportObj = [
+    {
+        Name: "Banti Picup",
+        number: "998734327"
+    },
+    {
+        Name: "sakir Picup",
+        number: "9351352598"
+    },
+
+    {
+        Name: "Rajinder Picup",
+        number: "9001526397"
+    },
+    {
+        Name: "Rinku Trnspoart",
+        number: "9024087606"
+    },
+
+    {
+        Name: "Bhura Picup",
+        number: "6376206106"
+    },
+]
